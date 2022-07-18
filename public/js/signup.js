660bee17cde97feaacdebb4fd2a4ff9630ff7c8b
diabm1 <diabm1855@gmail.com>
2022-07-18 00:05:50 -0400

    added signup formhandler logic

---
+++ public/js/signup.js
@@ -0,0 +1,25 @@
+const signupFormHandler = async function (e) {
+  e.preventDefault();
+
+  const usernameEl = document.querySelector("#username-input-signup").value;
+  const passwordEl = document.querySelector("#password-input-signup").value;
+
+  fetch("/api/user", {
+    method: "post",
+    body: JSON.stringify({
+      username: usernameEl.value,
+      password: passwordEl.value,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then(function () {
+      document.location.replace("/dashboard");
+    })
+    .catch((err) => console.log(err));
+};
+
+document
+  .querySelector("#signup-form")
+  .addEventListener("submit", signupFormHandler);
