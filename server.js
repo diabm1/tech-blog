c83eca0e6e31c2d1347ac835a34f362298142792
diabm1 <diabm1855@gmail.com>
2022-07-18 01:45:19 -0400

    still need to fix routes

--- server.js
+++ server.js
@@ -1,23 +1,39 @@
-const path = require('path');
+const path = require("path");
 const express = require("express");
-// const exphbs = require("express-handlebars");
-// const hbs = exphbs.create({});
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const helpers = require("./utils/helpers");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
-// const routes = require('./controllers/');
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-app.use(express.json())
-app.use(express.urlencoded({
-    extended: false
-}))
+const sess = {
+  secret: "secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
+app.use(session(sess));
 
-app.use(express.static(path.join(__dirname, 'public')));
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(require("./controllers/"));
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
-    // sequelize.sync({ force: false });
-  });
+  console.log(`App listening on port ${PORT}!`);
+  sequelize.sync({ force: false });
+});
